b3d8fd4b225bae4417a46f3708d78af4791b220d
Sreelekshmi S Amma <42946926+SreelekshmiNair@users.noreply.github.com>
2025-07-24 18:46:21 -0500

    Date : 07-24-2025 Problem : 1832-check-if-the-sentence-is-pangram Using : .py ------------------------------ Difficulty : Easy Time : 7 ms (5.45%) Space : 12.5 MB (51.76%)

--- LeetCode/Easy/1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ LeetCode/Easy/1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -4,11 +4,11 @@ class Solution(object):
         :type sentence: str
         :rtype: bool
         """
-        alphabet_set = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+        alphabet_set = set()
         for al in sentence:
-            if al.lower() in alphabet_set:
-                alphabet_set.remove(al.lower())
-        return len(alphabet_set)==0
+            if al.isalpha():
+                alphabet_set.add(al.lower())
+        return len(alphabet_set)==26
       
 
     
